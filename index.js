73795c1e84689742b06d7f6bdf4ecbe07f1c806f
dev_MD <137388933+devMainak@users.noreply.github.com>
2024-09-10 10:02:09 +0000

    feat: api for fetching books

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ const app = express();
 require("dotenv").config();
 
 const { initializeDatabase } = require("./db/db.connection");
+const { Movies } = require("./models/movie.model");
+
+app.use(cors());
+app.use(express.json());
+
+initializeDatabase();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.get("/movies", async (req, res) => {
+  try {
+    const allMovies = await Movies.find();
+    res.json(allMovies);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
